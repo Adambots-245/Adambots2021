b82e5948b48e8f5518b1114c7532c5efb148eabf
Cole VanOphem <njwv2003@gmail.com>
2021-02-26 18:04:14 -0500

    Added Shark Macro and dependencies

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -19,6 +19,10 @@ import frc.robot.Constants;
 import frc.robot.sensors.Gyro;
 import frc.robot.utils.Log;
 
+import org.hammerhead226.sharkmacro.*;
+import org.hammerhead226.sharkmacro.motionprofiles.ProfileRecorder;
+import org.hammerhead226.sharkmacro.motionprofiles.ProfileRecorder.RecordingType;
+
 public class DriveTrainSubsystem extends SubsystemBase {
   /**
    * Creates a new DriveTrainNew.
@@ -37,6 +41,9 @@ public class DriveTrainSubsystem extends SubsystemBase {
   private Gyro gyro;
 
   private boolean hasGyroBeenReset = false;
+
+  
+  private ProfileRecorder recorder;
 
   public DriveTrainSubsystem(Gyro gyro, Solenoid gearShifter, WPI_TalonFX frontRightMotor, WPI_TalonFX frontLeftMotor, WPI_TalonFX backLeftMotor, WPI_TalonFX backRightMotor) {
     super();
