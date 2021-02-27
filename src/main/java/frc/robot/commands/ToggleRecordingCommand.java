f6519450cb1b56d52f78f1eb6776740d3af20563
245 Drive Station <nvanop4420@rcs-k12.us>
2021-02-27 12:11:40 -0500

    Changed ToggleRecordingCommand to Command InstantCommand did not work

--- src/main/java/frc/robot/commands/ToggleRecordingCommand.java
+++ src/main/java/frc/robot/commands/ToggleRecordingCommand.java
@@ -1,12 +1,11 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.command.InstantCommand;
-import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
+import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrainSubsystem;
+import frc.robot.utils.Log;
 
-public class ToggleRecordingCommand extends InstantCommand {
+public class ToggleRecordingCommand extends Command {
     
     private DriveTrainSubsystem driveTrainSubsystem;
 
@@ -20,7 +19,13 @@ public class ToggleRecordingCommand extends InstantCommand {
 
     // Called once when the command executes
 	protected void initialize() {
+        super.initialize();
 		driveTrainSubsystem.toggleProfileRecording();
-	}
+    }
+    
+    public boolean isFinished() {
+        Log.info("ToggleRecordingCommand.isFinished()");
+        return true;
+    }
 
 }
