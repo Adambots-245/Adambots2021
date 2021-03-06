d983bddef47d11400dde14ab237b6c40543f4453
Cannicide <41762964+Cannicide@users.noreply.github.com>
2021-03-06 09:56:44 -0500

    Added PhotoEye and Conveyor code onto autonTesting code.

--- src/main/java/frc/robot/subsystems/ConveyorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ConveyorSubsystem.java
@@ -11,6 +11,9 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotMap;
+import frc.robot.sensors.PhotoEye;
 import frc.robot.utils.Log;
 
 public class ConveyorSubsystem extends SubsystemBase {
@@ -20,29 +23,69 @@ public class ConveyorSubsystem extends SubsystemBase {
   private WPI_VictorSPX conveyorMotor;
   private WPI_VictorSPX alignmentBeltMotor;
 
-  public ConveyorSubsystem(WPI_VictorSPX conveyorMotor, WPI_VictorSPX alignmentBeltMotor) {
+  private PhotoEye intakePhotoEye;
+  private PhotoEye spacingPhotoEye;
+  // private PhotoEye finalPhotoEye;
+
+  private boolean didSpacingEyePreviouslyDetectBall = false;
+  private int ballsPresent = 3;
+
+  public ConveyorSubsystem(WPI_VictorSPX conveyorMotor, WPI_VictorSPX alignmentBeltMotor, PhotoEye intakePhotoEye, PhotoEye spacingPhotoEye, PhotoEye finalPhotoEye) {
     super();
     
     this.conveyorMotor = conveyorMotor; //new WPI_VictorSPX(Constants.INFEED_CONVEYOR_MOTOR_PORT);
     this.alignmentBeltMotor = alignmentBeltMotor; //new WPI_VictorSPX(Constants.INFEED_CONVEYOR_INDEXER_MOTOR_PORT);
 
+    this.intakePhotoEye = intakePhotoEye;
+    this.spacingPhotoEye = spacingPhotoEye;
+    // this.finalPhotoEye = finalPhotoEye;
+
     Log.info("Initializing Conveyor");
   }
 
-  public void runConveyor(double speed){
+  public void runConveyor(double speed, boolean override){
+    if(!override){
+      if(ballsPresent == 5 || (spacingPhotoEye.isDetecting() && !didSpacingEyePreviouslyDetectBall)){
+        stopConveyorMotor();
 
-    //Log.infoF("Running coveyor. %% Output: %f", speed);
-    conveyorMotor.set(ControlMode.PercentOutput, speed);
+        if (spacingPhotoEye.isDetecting())
+          didSpacingEyePreviouslyDetectBall = true;
+      } else {
+
+        if(intakePhotoEye.isDetecting()){
+          conveyorMotor.set(ControlMode.PercentOutput, speed);
+        }
+      }
+    }
+    else{
+      conveyorMotor.set(ControlMode.PercentOutput, speed);
+    }
+
+    //Log.infoF("Running conveyor. %% Output: %f", speed);
+  }
+
+  public void stopConveyorMotor(){
+
+    conveyorMotor.set(ControlMode.PercentOutput, Constants.STOP_MOTOR_SPEED);
+
   }
 
   public void runAlignmentBelt(double speed){
 
-    //Log.infoF("Running Alignment Belt. %% Output: %f", speed);
     alignmentBeltMotor.set(ControlMode.PercentOutput, speed);
+
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    ballsPresent = 3 + (RobotMap.IntakeCounter.get() - RobotMap.ExitCounter.get()) / 2;
+    if (didSpacingEyePreviouslyDetectBall){
+      if (!spacingPhotoEye.isDetecting()){
+        didSpacingEyePreviouslyDetectBall = false;
+      }
+    }
+    
   }
 }
