c797ffcda412af6fd70d55b05c14d67b4f0d4bfd
raje2v <rajeevbiz@gmail.com>
2021-03-23 17:33:11 -0400

    Added multiple Turn Commands - NoPID and Turn by Rotational Distance (TurnDegreesCommand)

--- src/main/java/frc/robot/commands/autonCommands/TurnToAngleNoPIDCommand.java
+++ src/main/java/frc/robot/commands/autonCommands/TurnToAngleNoPIDCommand.java
@@ -11,39 +11,26 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.sensors.Gyro;
 import frc.robot.subsystems.DriveTrainSubsystem;
-import frc.robot.subsystems.GyroPIDSubsystem;
 
 public class TurnToAngleNoPIDCommand extends CommandBase {
   /**
    * Creates a new DriveForwardDistance.
    */
   DriveTrainSubsystem driveTrain;
-  double speed;
-  private GyroPIDSubsystem gyroPIDSubsystem;
   private Gyro gyro;
   private double targetAngle;
   private boolean resetGyro = true;
-  private double inputSpeed;
+  private double inputSpeed = 0.8; //don't turn too fast
 
-  public TurnToAngleNoPIDCommand(DriveTrainSubsystem inpuDriveTrain, double inputSpeed,
-      double targetAngle, boolean resetGyro) {
+  public TurnToAngleNoPIDCommand(DriveTrainSubsystem inpuDriveTrain, double targetAngle) {
     // Use addRequirements() here to declare subsystem dependencies.
     driveTrain = inpuDriveTrain;
-    // speed = inputSpeed;
-    speed = 0;
 
     this.targetAngle = targetAngle;
-    this.inputSpeed = inputSpeed;
 
     // gyroPIDSubsystem = new GyroPIDSubsystem();
     gyro = Gyro.getInstance();
     addRequirements(driveTrain);
-
-  }
-
-  TurnToAngleNoPIDCommand(DriveTrainSubsystem inpuDriveTrain, double inputSpeed,
-      double targetAngle) {
-    this(inpuDriveTrain, inputSpeed, targetAngle, true);
   }
 
   // Called when the command is initially scheduled.
@@ -52,13 +39,16 @@ public class TurnToAngleNoPIDCommand extends CommandBase {
     if (resetGyro) {
       gyro.reset();
     }
+
+    this.targetAngle += gyro.getYaw(); //if not set to zero, adjust to whatever the initial value is
+
     driveTrain.resetEncoders();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double turnSpeed = inputSpeed * (targetAngle/Math.abs(targetAngle)); //gyroPIDSubsystem.getController().calculate(gyroPIDSubsystem.getMeasurement(), targetAngle);
+    double turnSpeed = inputSpeed * Math.signum(targetAngle); 
     System.out.println("executing turn to angle");
     System.out.println("yaw:" + gyro.getYaw());
     SmartDashboard.putNumber("yaw", gyro.getYaw());
@@ -69,7 +59,7 @@ public class TurnToAngleNoPIDCommand extends CommandBase {
     SmartDashboard.putNumber("leftSpeed", driveTrain.getLeftDriveEncoderVelocity());
     SmartDashboard.putNumber("rightSpeed", driveTrain.getRightDriveEncoderVelocity());
 
-    driveTrain.arcadeDrive(speed, turnSpeed);
+    driveTrain.arcadeDrive(0, turnSpeed);
     // driveTrain.driveDistance(distance);
   }
 
@@ -79,6 +69,8 @@ public class TurnToAngleNoPIDCommand extends CommandBase {
     if (interrupted) {
       System.out.println("turn to angle interrupted");
     }
+
+    driveTrain.arcadeDrive(0,0);
   }
 
   // Returns true when the command should end.
